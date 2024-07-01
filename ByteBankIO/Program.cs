d6394a34d9be6589800491a46cebf1d4721f8d0c
Davi Monteiro <davi321522@gmail.com>
2024-07-01 15:50:58 -0300

    att06

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -18,14 +18,31 @@ class Program
         //public override int Read(byte[] array, int offset, int count);
         //Paramter:
         //array; armazenar os bytes que ja foram lidos.
-        //int offset; É o índice  que vai deliminar o array do nosso método.
+        //int offset; É o índice  que vai delimitar o array do nosso método.
         // int count; informa quantas posições queremos preencher em nosso array do método Read.
 
         //criamos nosso array de byte de 1Kb.
         var buffer = new byte[1024]; //quantidade de 1Kb.
 
         fluxoDoArquivo.Read(buffer,0,1024);
-        Console.WriteLine();
+        Console.WriteLine("GG ###");
+
+
+        //aqui criamos um método para escrever o nosso Buffer para assim entendermos melhor seu funcionamento
+        static void EscreverBuffer(byte[] buffer)
+        {
+               foreach (var meuByte in buffer)
+                  {
+                     Console.Write(meuByte);
+                     Console.Write(" ");
+                  }
+        }
+
+        EscreverBuffer(buffer);
+
+        Console.WriteLine("\n");
+        Console.WriteLine("digite A e depois ''Enter'' para finalizar");
+        
 
     }
 }
